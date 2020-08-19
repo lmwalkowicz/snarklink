9b07be43f8517b2338ef55bf62bc9ca13ed3b7f4
Lucianne Walkowicz <lwalkowicz@A0000000MQ.local>
2020-08-19 12:08:16 -0500

    initial commit of (in progress) scraper notebook

---
+++ config_example.py
@@ -0,0 +1,7 @@
+# .gitignore should include reference to config.py
+# SAVE THIS FILE AS CONFIG.PY
+consumer_key = "YOUR_CONSUMER_KEY"
+consumer_secret = "YOUR_CONSUMER_SECRET"
+access_token = "YOUR_ACCESS_TOKEN"
+access_token_secret = "YOUR_ACCESS_TOKEN_SECRET"
+
